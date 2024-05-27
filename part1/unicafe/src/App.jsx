b52a96c21206f1518a1e83102ebc9903ad126321
tuome <tonni@tuta.io>
2024-05-27 13:57:41 +0300

    Add part1 exercise 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToValue = (answer, value) => {
+    if (answer === 'good') {
+      setGood(value)
+    } else if (answer === 'neutral') {
+      setNeutral(value)
+    } else if (answer === 'bad') {
+      setBad(value)
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setToValue('good', good + 1)}>good</button>
+      <button onClick={() => setToValue('neutral', neutral + 1)}>neutral</button>
+      <button onClick={() => setToValue('bad', bad + 1)}>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
