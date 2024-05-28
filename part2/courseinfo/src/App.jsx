1bef4cc7bf053b00a06e2f16ff7edeb7a8097c8e
tuome <tonni@tuta.io>
2024-05-28 03:41:28 +0300

    Add part2 exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,26 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
-
-const Course = ({ course }) => {
-  const { name, parts } = course
-  const sum = parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
-
-  return (
-    <>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total sum={sum} />
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
