2d0ff092ae01a5906c0f83de767b80e635edb4f4
tuome <tonni@tuta.io>
2024-05-28 03:30:08 +0300

    Add part2 exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+const Course = ({ course }) => {
+  const { name, parts } = course
+  const sum = parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
+
+  return (
+    <>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total sum={sum} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
