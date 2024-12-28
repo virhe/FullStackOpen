be537ef737d5c10bc2d06bf6265a1cefbf16d7a1
tuome <tonni@tuta.io>
2024-12-28 13:54:44 +0200

    Add exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -23,6 +24,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {(good / total) * 100}%</p>
     </div>
   )
 }
